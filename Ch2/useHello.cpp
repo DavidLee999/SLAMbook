9f39fb23ddba054e160274631d8b56ce3a890d8c
李芃桦 <11186593+DavidLee999@users.noreply.github.com>
2018-01-01 21:20:51 +0100

    update

--- Ch2/useHello.cpp
+++ Ch2/useHello.cpp
@@ -1,8 +1,14 @@
 #include "libHelloSLAM.h"
+#include <iostream>
 
 int main(int argc, char** argv)
 {
     printHello();
 
+    int a = 0, b = 1;
+
+    a += b;
+
+    std::cout << a << std::endl;
     return 0;
 }
