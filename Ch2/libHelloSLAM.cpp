15aaa173cfb22d278e111a4fe469db0354dba97f
李芃桦 <11186593+DavidLee999@users.noreply.github.com>
2018-01-01 14:17:14 +0100

    Ch2

---
+++ Ch2/libHelloSLAM.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+void printHello()
+{
+    cout << "Hello SLAM" << endl;
+}
---
+++ Ch2/useHello.cpp
@@ -0,0 +1,8 @@
+#include "libHelloSLAM.h"
+
+int main(int argc, char** argv)
+{
+    printHello();
+
+    return 0;
+}
